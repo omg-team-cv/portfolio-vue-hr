5c1558476be2b4cc26bb05514041d4979d488cf7
Akatocha <66845412+Akatocha@users.noreply.github.com>
2020-06-15 00:34:18 +0300

    Initial commit

---
+++ dist/precache-manifest.28d01fbc289f35c5edf1737e5dbb5eae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9cf6219eb0341a64bc5",
+    "url": "/css/app.eca8e225.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "76ad9b103d33cfbafb8bd30bbf317889",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24bf92d9c47980afd533",
+    "url": "/js/about.26d57fae.js"
+  },
+  {
+    "revision": "c9cf6219eb0341a64bc5",
+    "url": "/js/app.a305017d.js"
+  },
+  {
+    "revision": "a10e28744c68f701947d",
+    "url": "/js/chunk-vendors.09fdc3f8.js"
+  },
+  {
+    "revision": "0438f1bcbb6e938b4a563f3ff560c59c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
